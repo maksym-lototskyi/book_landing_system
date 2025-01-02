d0cbd2d759f279b3bb4da788dbdb0e7c324b4325
maksym-lototskyi <lototskyimaksym@gmail.com>
2025-01-02 22:02:44 +0200

    version 1.1

--- src/main/java/com/api/book_landing_system/service/AuthenticationService.java
+++ src/main/java/com/api/book_landing_system/service/AuthenticationService.java
@@ -1,9 +1,7 @@
 package com.api.book_landing_system.service;
 
-import com.api.book_landing_system.dtos.user_dtos.LoginDto;
 import com.api.book_landing_system.dtos.user_dtos.RegisterDto;
 
-public interface AuthorizationService {
+public interface AuthenticationService {
     void registerUser(RegisterDto registerDto);
-    void loginUser(LoginDto loginDto);
 }
